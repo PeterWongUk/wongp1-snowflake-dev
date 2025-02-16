59b52795a87e205e282af03444eb5df55edb9a93
peter_wong_uk <peter_wong_uk@yahoo.com>
2025-02-16 07:47:25 +0000

    adjust

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = ">= 1.0.0"
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
     }
   }
 
